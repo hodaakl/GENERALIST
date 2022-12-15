6324f6a351cae9ff3ce293e030f0836b58827dad
Akl2 <akl.2@osu.edu>
2022-12-15 12:58:10 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,43 +1,21 @@
-"""In this script you can run the function "generalist" -- optimization uses adam algorithm
-    Input:
-    fasta_path:  fasta file (msa) of protein family 
-    k : latent dimension used in the model
-    out_dir : output directory : if doesn't exist the function will create that directory
-    save_step (default 100): save the parameters every save_step steps , in case the jobs got interrupted 
-    thresh (default 1) : inference steps when |grad(Z)|/|Z| is less than thresh , same for theta  # threshold for stopping 
-    alpha (default .01) : for the adaptive learning rate
-    steps (default 10^7 ) : maximum number of allowed steps
-
-    Outputs: 
-    Written as files in the output directory: 
-        Z: written as numpy and torch array in the output directory
-        Theta: written as numpy and torch array in the output directory
-        details: saves details of the inference  
-"""
-############################################ RUN INFERENCE #####################################################
-from compile_generalist_fn import generalist
+"""In this script you can run inference on model "generalist" -- optimization uses adam algorithm"""
+############################################ ONE HOT ENCODE DATA #####################################################
 from data_process_fns import Convert_fastaToNp
-import numpy as np
-# necessary arguments for generalist
 FastaFilePath  = '../data/msa_p53_unimsa.fa' # file path of the fasta file 
-# convert the fasta file to one-hot-encoded matrix -- if the MSA file has sequences without labels (no > label line )
-# before the sequence, then set labels_inc =False 
-one_hot_msa = Convert_fastaToNp(filepath = FastaFilePath, binary = True, labels_inc =True)
-k= 7 # decide on the latent dimension for the model
-output_directory = f'../p53_output_k{k}/' # the output directory where the parameters will be saved 
-#This function should detect a gpu if available, otherwise runs on cpu
-generalist(sigmas = one_hot_msa, k = k, out_dir = output_directory) # 
-# parameters are saved in the directory output_directory 
+data_one_hot = Convert_fastaToNp(filepath = FastaFilePath, binary = True, labels_inc =True)
+############################################ RUN INFERENCE #####################################################
+from generalist_class import Generalist
+k= 32 # decide on the latent dimension for the model
+generalist = Generalist(data_one_hot,k )# parameters are saved in the directory output_directory 
+generalist.train() #use_gpu_if_avail = True
 ############################################ GENERATE NEW SEQUENCES ############################################
-from loader_class import Generator
 ngen = 1000
-Gen_obj = Generator(output_directory, k = k)
-generated_data = Gen_obj.GenData(ngen) #numpy array
+generated_data = generalist.generate(nGen = ngen) # output_binary = False
 print(generated_data.shape)
 ### save the generated dataset as numpy matrix 
 file_name = f'generated_data.npy'
-output_fn = f'{output_directory}{file_name}'
-np.save(output_fn, generated_data)
+import numpy as np
+np.save(file_name, generated_data)
 ### save the generated dataset as a fasta file 
-from data_process_fns import Conv_save_NpToFasta
-Conv_save_NpToFasta(generated_data, path= output_directory + f'generated_set_k{k}.fa',  label = 'generated_seq', verbose = False , save = True, include_fish_sym = True)
+# from data_process_fns import Conv_save_NpToFasta
+# Conv_save_NpToFasta(generated_data, path=f'generated_set_k{k}.fa',  label = 'generated_seq', verbose = False , save = True, include_fish_sym = True)
